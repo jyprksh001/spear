94c481d21a760d48ee1b852a16668c6327c160f6
hexdiract <octeep@pm.me>
2020-03-11 17:49:22 +0800

    Added comments

--- cmd/spear-frontend/config/config.go
+++ cmd/spear-frontend/config/config.go
@@ -1,112 +1,121 @@
 package config
 
 import (
-    "encoding/json"
-    "encoding/base64"
-    "strings"
-    "strconv"
-    "fmt"
-    "net"
-    "io/ioutil"
+	"encoding/base64"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net"
+	"strconv"
+	"strings"
 )
 
+//ParseAddr turns a string in ipv4:port format to a UDPAddr
 func ParseAddr(str string) (*net.UDPAddr, error) {
-    result := strings.Split(str, ":")
+	result := strings.Split(str, ":")
 
-    var ipstr, portstr string
-    if len(result) == 2 {
-        ipstr = result[0]
-        portstr = result[1]
-    } else {
-        return nil, fmt.Errorf("Failed to parse Address: %#v, length of split result is %d", str, len(result))
-    }
+	var ipstr, portstr string
+	if len(result) == 2 {
+		ipstr = result[0]
+		portstr = result[1]
+	} else {
+		return nil, fmt.Errorf("Failed to parse Address: %#v, length of split result is %d", str, len(result))
+	}
 
-    ip := net.ParseIP(ipstr)
-    if ip == nil {
-        return nil, fmt.Errorf("Failed to parse IP: %#v", ipstr)
-    }
+	ip := net.ParseIP(ipstr)
+	if ip == nil {
+		return nil, fmt.Errorf("Failed to parse IP: %#v", ipstr)
+	}
 
-    port, err := strconv.Atoi(portstr)
-    if err != nil {
-        return nil, fmt.Errorf("Failed to parse Port: %#v", portstr)
-    }
+	port, err := strconv.Atoi(portstr)
+	if err != nil {
+		return nil, fmt.Errorf("Failed to parse Port: %#v", portstr)
+	}
 
-    return &net.UDPAddr{IP: ip, Port: port}, nil
+	return &net.UDPAddr{IP: ip, Port: port}, nil
 }
 
-
+//Key refers to a public key or a secret key, used for marshalling
 type Key struct {
-    Bytes []byte
+	Bytes []byte
 }
 
+//MarshalJSON is used for JSON marshalling
 func (key *Key) MarshalJSON() ([]byte, error) {
-    return json.Marshal(base64.StdEncoding.EncodeToString(key.Bytes))
+	return json.Marshal(base64.StdEncoding.EncodeToString(key.Bytes))
 }
 
+//UnmarshalJSON is used for JSON unmarshalling
 func (key *Key) UnmarshalJSON(bytes []byte) error {
-    fmt.Println("What")
+	fmt.Println("What")
 
-    var encoded string
-    if err := json.Unmarshal(bytes, &encoded); err != nil {
-        return err
-    }
+	var encoded string
+	if err := json.Unmarshal(bytes, &encoded); err != nil {
+		return err
+	}
 
-    decoded, err := base64.StdEncoding.DecodeString(encoded)
-    if err != nil {
-        return err
-    }
-    if len(decoded) != 32 {
-        return fmt.Errorf("Decoded key has byte length %d", len(decoded))
-    }
+	decoded, err := base64.StdEncoding.DecodeString(encoded)
+	if err != nil {
+		return err
+	}
+	if len(decoded) != 32 {
+		return fmt.Errorf("Decoded key has byte length %d", len(decoded))
+	}
 
-    key.Bytes = decoded
-    return nil
+	key.Bytes = decoded
+	return nil
 }
 
+//Address is a proxy struct used for JSON marshalling
 type Address struct {
-    UDP net.UDPAddr
+	UDP net.UDPAddr
 }
 
+//MarshalJSON is used for JSON marshalling
 func (addr *Address) MarshalJSON() ([]byte, error) {
-    return json.Marshal(addr.UDP.String())
+	return json.Marshal(addr.UDP.String())
 }
 
+//UnmarshalJSON is used for JSON unmarshalling
 func (addr *Address) UnmarshalJSON(bytes []byte) error {
-    fmt.Println("WHAT")
-    var str string
-    if err := json.Unmarshal(bytes, &str); err != nil {
-        return err
-    }
+	fmt.Println("WHAT")
+	var str string
+	if err := json.Unmarshal(bytes, &str); err != nil {
+		return err
+	}
 
-    a, err := ParseAddr(str)
-    if err != nil {
-        return err
-    }
+	a, err := ParseAddr(str)
+	if err != nil {
+		return err
+	}
 
-    addr.UDP.IP = a.IP
-    addr.UDP.Port = a.Port
-    return nil
+	addr.UDP.IP = a.IP
+	addr.UDP.Port = a.Port
+	return nil
 }
 
+//Peer is a struct used for JSON marshalling, not to be confused with network.Peer
 type Peer struct {
-    PublicKey Key `json:"pk"`
-    Candidates []Address `json:"candidates"`
+	PublicKey  Key       `json:"pk"`
+	Candidates []Address `json:"candidates"`
 }
 
+//Config is a struct used for JSON marshalling, mirroring the structure of config.json
 type Config struct {
-    SecretKey Key `json:"sk"`
-    Candidates []Address `json:"candidates"`
-    Peers []Peer `json:"peers"`
+	SecretKey  Key       `json:"sk"`
+	Candidates []Address `json:"candidates"`
+	Peers      []Peer    `json:"peers"`
 }
 
+//ReadFile reads a config.json and init the values of conf
 func (conf *Config) ReadFile(path string) error {
-    data, err := ioutil.ReadFile("config.json")
-    if err != nil {
-        return err
-    }
+	data, err := ioutil.ReadFile("config.json")
+	if err != nil {
+		return err
+	}
 
-    if err := json.Unmarshal(data, &conf); err != nil {
-        return err
-    }
-    return nil
+	if err := json.Unmarshal(data, &conf); err != nil {
+		return err
+	}
+	return nil
 }
--- cmd/spear/network/network.go
+++ cmd/spear/network/network.go
@@ -1,138 +1,146 @@
 package network
 
 import (
-    "errors"
-    "net"
-    "github.com/google/go-cmp/cmp"
-    "bytes"
-    "fmt"
-    "../crypto"
+	"bytes"
+	"errors"
+	"fmt"
+	"net"
+
+	"../crypto"
 )
 
-const (
-    DEFAULT_PORT int = 57975
-)
-
-// Addr: a container of candidates and the current one
+// Addr is a container of candidates and the current one
 type Addr struct {
-    current *net.UDPAddr
-    Candidates []*net.UDPAddr
+	current    *net.UDPAddr
+	Candidates []*net.UDPAddr
 }
 
+//Bind tries to bind the client to one of the address
 func (addr *Addr) Bind() (*net.UDPConn, error) {
-    for _, cand := range addr.Candidates {
-        conn, err := net.ListenUDP("udp", cand)
-        if err == nil {
-            addr.current = cand
-            return conn, nil
-        }
-    }
-    return nil, errors.New("Unable to bind to any candidates")
+	for _, cand := range addr.Candidates {
+		conn, err := net.ListenUDP("udp", cand)
+		if err == nil {
+			addr.current = cand
+			return conn, nil
+		}
+	}
+	return nil, errors.New("Unable to bind to any candidates")
 }
 
+//Set sets the current UDPAddr used by the Client
 func (addr *Addr) Set(currentNew *net.UDPAddr) error {
-    for _, cand := range addr.Candidates {
-        if cmp.Equal(cand, currentNew) {
-            addr.current = currentNew
-            return nil
-        }
-    }
-    return errors.New("UDPAddr is not in candidate list")
+	for _, cand := range addr.Candidates {
+		if cand.IP.Equal(currentNew.IP) && cand.Port == currentNew.Port {
+			addr.current = currentNew
+			return nil
+		}
+	}
+	return errors.New("UDPAddr is not in candidate list")
 }
 
+//Get returns the current address
 func (addr *Addr) Get() *net.UDPAddr {
-    return addr.current
+	return addr.current
 }
 
 func (addr *Addr) Write(conn *net.UDPConn, data []byte) {
-    if addr.current == nil {
-        for _, cand := range addr.Candidates {
-            conn.WriteToUDP(data, cand)
-        }
-    } else {
-        conn.WriteToUDP(data, addr.current)
-    }
+	if addr.current == nil {
+		for _, cand := range addr.Candidates {
+			conn.WriteToUDP(data, cand)
+		}
+	} else {
+		conn.WriteToUDP(data, addr.current)
+	}
 }
 
+//Packet refers to a decrypted incoming packet sent by a peer
 type Packet struct {
-    ID uint64
-    Data []byte
+	ID   uint64
+	Data []byte
 }
 
+//Peer refers to another spear user
 type Peer struct {
-    PublicKey []byte
-    Addr Addr
+	PublicKey []byte
+	Addr      Addr
 }
 
+//Client refers to the backend of the client containing all basic information needed by the core
 type Client struct {
-    SecretKey []byte
-    Nonce []byte
+	SecretKey []byte
+	nonce     []byte
 
-    PeerList []*Peer
+	PeerList []*Peer
 
-    Addr Addr
-    conn *net.UDPConn
+	Addr Addr
+	conn *net.UDPConn
 
-    Callback *func(*Peer, *Packet)
+	Callback *func(*Peer, *Packet)
 }
 
+//Initialize setup the client, should be called first
 func (client *Client) Initialize() error {
-    conn, err := client.Addr.Bind()
-    if err != nil {
-        return err
-    }
-    conn.SetReadBuffer(0x100000)
-    client.conn = conn
-    return nil
+	crypto.Init()
+	conn, err := client.Addr.Bind()
+	if err != nil {
+		return err
+	}
+	conn.SetReadBuffer(0x100000)
+	client.conn = conn
+	client.nonce = crypto.RandomBytes(crypto.NonceSize)
+	return nil
 }
 
+//Start starts listening to incoming packets
 func (client *Client) Start() {
-    for {
-        buffer := make([]byte, 0x1000)
-        size, addr, err := client.conn.ReadFromUDP(buffer)
-        if err != nil {
-            fmt.Println(err)
-            continue
-        }
+	for {
+		buffer := make([]byte, 0x1000)
+		size, addr, err := client.conn.ReadFromUDP(buffer)
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
 
-        pk, id, plaintext, err := crypto.DecryptBytes(buffer[:size], client.SecretKey)
-        if err != nil {
-            fmt.Println(err)
-            continue
-        }
+		pk, id, plaintext, err := crypto.DecryptBytes(buffer[:size], client.SecretKey)
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
 
-        sender, ok := client.GetPeerByPublicKey(pk)
-        if err := sender.Addr.Set(addr); err != nil {
-            panic(err)
-        } else {
-            if !ok {
-                sender = &Peer{
-                    PublicKey: pk,
-                    Addr: Addr{
-                        Candidates: []*net.UDPAddr{addr},
-                    },
-                }
-                sender.Addr.Set(addr)
-            }
-        }
+		sender := client.GetPeerByPublicKey(pk)
+		if err := sender.Addr.Set(addr); err != nil {
+			panic(err)
+		} else {
+			if sender == nil {
+				sender = &Peer{
+					PublicKey: pk,
+					Addr: Addr{
+						Candidates: []*net.UDPAddr{addr},
+					},
+				}
+				sender.Addr.Set(addr)
+			}
+		}
 
-        go (*client.Callback)(sender, &Packet{
-            ID: id,
-            Data: plaintext,
-        })
-    }
+		go (*client.Callback)(sender, &Packet{
+			ID:   id,
+			Data: plaintext,
+		})
+	}
 }
 
+//SendBytes send a packet to another peer, bytes should be unencrypted.
 func (client *Client) SendBytes(peer *Peer, bytes []byte) {
-    ciphertext := crypto.EncryptBytes(peer.PublicKey, client.SecretKey, bytes, client.Nonce)
-    peer.Addr.Write(client.conn, ciphertext)
+	ciphertext := crypto.EncryptBytes(peer.PublicKey, client.SecretKey, bytes, client.nonce)
+	peer.Addr.Write(client.conn, ciphertext)
 }
 
-func (client *Client) GetPeerByPublicKey(pk []byte) (*Peer, bool) {
-    for _, peer := range client.PeerList {
-        if bytes.Compare(pk, peer.PublicKey) == 0 {
-            return peer, true
-        }
-    }
-    return nil, false
+//GetPeerByPublicKey attempts to find a peer with the corresponding public key. Returns nil if nothing is found.
+func (client *Client) GetPeerByPublicKey(pk []byte) *Peer {
+	for _, peer := range client.PeerList {
+		if bytes.Compare(pk, peer.PublicKey) == 0 {
+			return peer
+		}
+	}
+	return nil
 }
